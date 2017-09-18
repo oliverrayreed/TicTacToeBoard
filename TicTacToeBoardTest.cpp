fa38bd7874f59bf5d4de01003c9a06093c7c80b8
Dr Kevin Buffardi <kbuffardi@csuchico.edu>
2017-09-18 18:52:57 +0000

    Modified TicTacToeBoard constructor and added testcases.

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -20,3 +20,32 @@ TEST(TicTacToeBoardTest, sanityCheck)
 	ASSERT_TRUE(true);
 }
 */
+
+TEST(TicTacToeBoard_is_square, sanityCheck){
+	int sizex = TicTacToeBoard.size();
+	int sizey;
+	for (int i = 0; i < sizex; i++){
+		sizey = TicTacToeBoard[i].size();
+		ASSERT_EQ(sizex, sizey);
+	}
+}
+
+TEST(TicTacToeBoard_is_3_3, sanityCheck){
+	bool three = true;
+	int sizex = TicTacToeBoard.size();
+	int sizey;
+	
+	if (size x != 3)
+		
+	for (int i = 0; i < sizex; i++){
+		sizey = TicTacToeBoard[i].size();
+		ASSERT_EQ(sizey, 3);
+	}
+}
+
+TEST(TicTacToeBoard_is_empty, sanityCheck){
+	for (int i = 0; i < 3; i++)
+		for (int j =0; j < 3; j++)
+			ASSERT_EQ
+	
+}
